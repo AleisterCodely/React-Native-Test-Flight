bec8be4a47b029cfc2b09fa45fef892e38409dca
AleisterCodely <aleistercodely@gmail.com>
2020-12-14 11:42:46 -0600

    Playing with a reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,32 +1,43 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Text, StyleSheet, View, Button } from 'react-native';
 import ColorAdjustBit from '../Bits/ColorAdjustBit';
 
-const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
+const INCREMENT = 15;
 
-  const INCREMENT = 15;
+const SquareScreen = () => {
+    const reducer=(state, action)=>{
+        switch(action.colorToChange){
+            case 'red':
+                return {...state, red: state.red+action.amount};
+            case 'green':
+                return {...state, green: state.green+action.amount};
+            case 'blue':
+                return {...state, blue: state.blue+action.amount};
+            default:
+                return state;
+        }
+    };
+
+    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
 
   return (
     <View>
       <ColorAdjustBit
-        onIncrease={() => setRed(red + INCREMENT > 255 ? red + 0 : red + INCREMENT)}
-        onDecrease={() => setRed(red - INCREMENT <= 0 ? red - 0 : red - INCREMENT)}
+        onIncrease={() => dispatch({colorToChange: 'red', amount: INCREMENT})}
+        onDecrease={() => dispatch({colorToChange: 'red', amount: INCREMENT*-1})}
         color="Red"
       />
       <ColorAdjustBit
-        onIncrease={() => setGreen(green + INCREMENT > 255 ? green + 0 : green + INCREMENT)}
-        onDecrease={() => setGreen(green - INCREMENT <= 0 ? green - 0 : green - INCREMENT)}
+        onIncrease={() => dispatch({colorToChange: 'green', amount: INCREMENT})}
+        onDecrease={() => dispatch({colorToChange: 'green', amount: INCREMENT*-1})}
         color="Green"
       />
       <ColorAdjustBit
-        onIncrease={() => setBlue(blue + INCREMENT > 255 ? blue + 0 : blue + INCREMENT)}
-        onDecrease={() => setBlue(blue - INCREMENT <= 0 ? blue - 0 : blue - INCREMENT)}
+        onIncrease={() => dispatch({colorToChange: 'blue', amount: INCREMENT})}
+        onDecrease={() => dispatch({colorToChange: 'blue', amount: INCREMENT*-1})}
         color="Blue"
       />
-      <View style={{ height: 200, width: 200, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
+      <View style={{ height: 200, width: 200, backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})` }} />
     </View>
   );
 };
